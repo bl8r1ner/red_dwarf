1678e1b3db12361c217c8765d9b73d4642e8f1f8
bl8r1ner <81622969+bl8r1ner@users.noreply.github.com>
2025-04-03 13:06:55 +0300

    Update pushme.sh

--- pushme.sh
+++ pushme.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-
 # Check if a commit message is provided
 if [ -z "$1" ]; then
   echo "Error: No commit message provided."
   echo "Usage: ./pushme \"Your commit message\""
   exit 1
 fi
-
 
 # Use the provided commit message
 commit_message="$1"
